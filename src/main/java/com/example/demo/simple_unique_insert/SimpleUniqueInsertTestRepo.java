3bc829075b86de841d9d19fa7a686a03503f3d0e
Prateek Agrawal <prateek.agrawal@oyorooms.com>
2022-03-19 17:27:04 +0530

    commit

--- src/main/java/com/example/demo/simple_unique_insert/SimpleUniqueInsertTestRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.simple_unique_insert;
-
-import org.springframework.data.repository.CrudRepository;
-
-interface SimpleUniqueInsertTestRepo extends CrudRepository<SimpleUniqueInsertTest, Integer> {
-
-}
--- src/main/java/com/example/demo/simple_unique_update/SimpleUniqueUpdateTestRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.simple_unique_update;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-
-interface SimpleUniqueUpdateTestRepo extends CrudRepository<SimpleUniqueUpdateTest, Integer> {
-  List<SimpleUniqueUpdateTest> findByFirst(String first);
-}
