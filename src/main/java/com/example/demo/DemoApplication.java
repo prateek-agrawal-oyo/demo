459faf52536fe372a8e2410ab4c412520d2e78bf
Prateek Agrawal <prateek.agrawal@oyorooms.com>
2022-04-04 21:48:43 +0530

    Code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
+import java.io.FileReader;
+import java.io.IOException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.opencsv.CSVReader;
 
 @SpringBootApplication
 public class DemoApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+  public static void main(String[] args) throws IOException {
+    int count = 0;
+    try (CSVReader reader = new CSVReader(new FileReader("/Users/prateek.agarwal/Downloads/p.csv"))) {
+      while (reader.readNext() != null)
+        count++;
+      System.out.println(count);
+    }
   }
 
 }
