50dbf903b1b894cd31175d4d821f0fae66c2d801
prateek-agrawal-oyo <92846231+prateek-agrawal-oyo@users.noreply.github.com>
2022-05-24 17:04:39 +0530

    Revert "F1"

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,18 +3,14 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "//")
+@RestController(value = "/")
 public class Controller {
 
   @GetMapping("/ping")
   public String ping() {
-    return "pong2";
+    return "pong1";
   }
 
   line 141
   line 20
-  line 15
-  line 1
-  line 15 1
-  line 16
 }
